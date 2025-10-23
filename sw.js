8bf7cb025d8b105c905b22b156aea6e433938ed7
ClarkeTj <clarketejiri@gmail.com>
2025-10-23 08:28:31 +0100

    minor fix

--- sw.js
+++ sw.js
@@ -15,12 +15,7 @@ const ASSETS = [
   "/assets/js/matches.js", "/assets/js/script.js",
   "/manifest.json",
   "/assets/icons/fupreChessClub-icon-192.png",
-  "/assets/icons/fupreChessClub-icon-512.png",
-"/assets/css/all.min.css",
-"/assets/webfonts/fa-solid-900.woff2",
-"/assets/webfonts/fa-regular-400.woff2",
-"/assets/webfonts/fa-brands-400.woff2",
-"/assets/webfonts/fa-v4compatibility.woff2"
+  "/assets/icons/fupreChessClub-icon-512.png"
 
 ];
 
